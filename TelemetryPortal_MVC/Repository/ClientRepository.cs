134afe3c779a6a2b5baf53c33acc3a8762be41aa
Mpumie Khoza <32593392@mynwu.ac.za>
2024-08-19 22:25:18 +0200

    Project 3 Almost complete version

---
+++ TelemetryPortal_MVC/Repository/ClientRepository.cs
@@ -0,0 +1,18 @@
+﻿using TelemetryPortal_MVC.Data;
+using TelemetryPortal_MVC.Models;
+using System.Linq;
+namespace TelemetryPortal_MVC.Repository
+{
+    public class ClientRepository: GenericRepository<Client>, IClientRepository 
+    {
+        public ClientRepository(TechtrendsContext context): base(context) 
+        { 
+        }
+        public Client GetMostRecentClient()
+        {
+            return _context.Clients.OrderByDescending(clients => clients.CreatedDate).FirstOrDefault();
+        }
+
+
+    }
+}
---
+++ TelemetryPortal_MVC/Repository/ProjectRepository.cs
@@ -0,0 +1,18 @@
+﻿using TelemetryPortal_MVC.Data;
+using TelemetryPortal_MVC.Models;
+using System.Collections;
+using System.Linq;
+
+namespace TelemetryPortal_MVC.Repository
+    
+{
+    public class ProjectRepository
+    {
+        protected readonly TechtrendsContext _context = new TechtrendsContext();
+
+        public IEnumerable<Project> GetAll()
+        { 
+         return _context.Projects.ToList();
+        }
+    }
+}
