2efe350c19d97482c6843aa26a30ca92ee284dfe
Dee699 <DANIELMPUMELELO1@GMAIL.COM>
2024-09-27 23:12:44 +0200

    Update IClientRepository.cs

--- TelemetryPortal_MVC/Repository/IClientRepository.cs
+++ TelemetryPortal_MVC/Repository/IClientRepository.cs
@@ -6,8 +6,13 @@ namespace TelemetryPortal_MVC.Repository
     {
         // Calling methods created in the Client Repository
         Client GetClientById(Guid id);
+        
         IEnumerable<Client> GetClientsByName(string name);
+        
         IEnumerable<Client> GetClientsByContactEmail(string email);
+        
         IEnumerable<Client> GetClientsByDateOnboarded(DateTime dateOnboarded);
+    
+        Task<IEnumerable<Client>> GetAllAsync();
     }
 }
